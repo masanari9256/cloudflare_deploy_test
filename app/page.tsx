d606980db62e3b81ff4baf4df5a2d573b7d720a1
masanari9256 <masanari92562@gmail.com>
2025-06-12 21:46:43 +0900

    tailwindcssのcontainerをかいておく

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <div>
+        <div className="container mx-auto">
             <h1 className="text-3xl font-bold underline">
                 Hello world!
             </h1>
