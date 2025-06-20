2606c2b9adae5cc1bfc22efb34286c264d46dc18
masanari9256 <masanari92562@gmail.com>
2025-06-20 17:51:24 +0900

    背景変更してみた

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <div className="container mx-auto">
+        <div className="container mx-auto bg-red-300">
             <h1 className="text-3xl font-bold underline">
                 Hello cloudflare!!
             </h1>
